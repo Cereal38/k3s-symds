b2904e60d08dfb44fa23f94289d3e5fc6cd8723a
bruncosg <bruncosg@ill.fr>
2026-08-10 13:16:26 +0200

    Remove the seed postgres logic

--- seed-pg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# birth_year is voluntarily a 4 char string to demonstrate data transformation
-kubectl exec -i identity-pg-1 -- psql -U postgres -d identity \
-  -c "create table if not exists identity (id int PRIMARY KEY NOT NULL, first_name VARCHAR(255), last_name VARCHAR(255), birth_year VARCHAR(4));"
